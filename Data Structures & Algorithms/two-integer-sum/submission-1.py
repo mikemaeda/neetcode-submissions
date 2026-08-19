a93d5f2126001b9b9d11526bcd0f47009c4b18a4
Mike Maeda <mikemaeda7@gmail.com>
2026-08-19 14:35:25 -0400

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,46 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}
+        
+        for i, n in enumerate(nums):
+            diff = target - n 
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i 
+        return 
+# Intuition
+
+# My first thought was that for every number, 
+#I can calculate the number I need to reach the target.
+
+# If I’ve already seen that number before, then 
+#I’ve found my pair and can return their indices immediately.
+
+# Otherwise, I store the current number and its 
+# index so future numbers can use it as their match.
+
+# Approach
+
+# 1. Create an empty dictionary called prevMap.
+# 2. Loop through nums using both the index i and value n. use enumerate in python 
+# 3. Calculate diff = target - n.
+# 4. Check if diff already exists in prevMap.
+# 5. If it does, return the stored index and the current index.
+# 6. Otherwise, store the current number and its index in prevMap.
+# 7. Continue until the pair is found.
+
+# Complexity
+
+# * Time Complexity: O(n)
+# * Space Complexity: O(n)
+
+               
+
+            
+            
+
+
+
+
+
+
